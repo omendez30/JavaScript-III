00860361b58c5b6560ad8553a270f08034cf9410
omendez30 <omendez9@outlook.com>
2019-01-22 20:35:08 -0500

    explained the four principles of this, and wrote out one explanation

--- assignments/this.js
+++ assignments/this.js
@@ -1,17 +1,20 @@
 /* The for principles of "this";
-* in your own words. explain the four principle for the "this" keyword below.
-*
-* 1. 
-* 2. 
-* 3. 
-* 4. 
-*
-* write out a code example of each explanation above
-*/
+ * in your own words. explain the four principle for the "this" keyword below.
+ *
+ * 1. global binding, when using this in a global binding you get everything javascript has to offer within the language. It gives you javascript in its entirety.
+ * 2. implicit binding, when invoked, using dot notation, points to an object standing before the dot.
+ * 3. new binding, refers to constructors to create new objects for different instances/variable.
+ * 4. explicit binding, uses .call, .apply, .bind
+ *
+ * write out a code example of each explanation above
+ */
 
 // Principle 1
 
 // code example for Window Binding
+// function sayHello(name){
+//     console.log(this);
+// }
 
 // Principle 2
 
