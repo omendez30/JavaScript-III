51c1b11fd441c41d11aaba10dffb15ecb7d373bd
omendez30 <omendez9@outlook.com>
2019-01-22 20:49:00 -0500

    code example 3 new binding

--- assignments/this.js
+++ assignments/this.js
@@ -3,8 +3,8 @@
  *
  * 1. global binding, when using this in a global binding you get everything javascript has to offer within the language. It gives you javascript in its entirety.
  * 2. implicit binding, when invoked, using dot notation, points to an object standing before the dot.
- * 3. new binding, refers to constructors to create new objects for different instances/variable.
- * 4. explicit binding, uses .call, .apply, .bind
+ * 3. new binding, refers to constructors to create new objects and this keyword is bound the new object
+ * 4. explicit binding, uses .call, .apply, .bind which will reference the object from a global scope using this.
  *
  * write out a code example of each explanation above
  */
@@ -30,7 +30,13 @@ person.sayHello();
 // Principle 3
 
 // code example for New Binding
+let Animals = function(type, weight, name) {
+  this.type = type;
+  this.weight = weight;
+  this.name = name;
+};
 
+let liger = new Animals("lion/tiger hybrid", 200, "Leo");
 // Principle 4
 
 // code example for Explicit Binding
